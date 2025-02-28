e5854d49501a94fb5c583ee88a6e2ffe0f889f9d
Divided <CantStopDivided@proton.me>
2025-02-28 12:51:17 +0100

    Update and rename App.py to ZODDER.py

--- ZODDER.py
+++ ZODDER.py
@@ -1,18 +1,15 @@
-# Zodder
-# Developed by WiiCode.
-# The copy to clipboard button won't work on Replit, same for the open Google translate button.
+# ZODDER.py
+# created by ZAYD <3
+# linktr.ee/cantstopdivided
 
-# Import everything needed.
 import webbrowser
-
 try:
   import tkinter as tk
   import pyperclip
 except ImportError:
-  os.system("pip install -r Requirements.txt")
-  os.system("pip3 install -r Requirements.txt")
+  os.system("pip install -r requirements.txt")
+  os.system("pip3 install -r requirements.txt")
 
-# Create letters and symbols to use to encrypt.
 zod_letters = {
     'J': 'A',
     'B': 'B',
@@ -42,10 +39,8 @@ zod_letters = {
     'L': 'Z'
 }
 
-# Know real letters.
 english_letters = {v: k for k, v in zod_letters.items()}
 
-# Create a sub that converts a statement to Zod.
 def convert_to_zod(word):
     converted_word = ''
     for letter in word:
@@ -55,7 +50,6 @@ def convert_to_zod(word):
             converted_word += letter
     return converted_word
 
-# Create a sub that reconverts Zod.
 def convert_to_english(word):
     converted_word = ''
     for letter in word:
@@ -65,12 +59,10 @@ def convert_to_english(word):
             converted_word += letter
     return converted_word
 
-# Create a function to copy text to the clipboard
 def copy_to_clipboard():
     converted_text = output_label.cget("text")
     pyperclip.copy(converted_text)
 
-# Create a button to convert.
 def convert_button_click():
     input_word = entry.get()
     if conversion_mode.get() == 1:
@@ -79,12 +71,9 @@ def convert_button_click():
         output_word = convert_to_zod(input_word)
     output_label.config(text=output_word)
 
-# Create a function to go to Google translate.
-# This function also won't work in Replit.. wonder if that's because you're not on a local machine.
 def open_translate_website():
     webbrowser.open_new_tab('https://translate.google.com/')
 
-# Create the GUI window.
 window = tk.Tk()
 window.title('Zod Language Converter')
 entry = tk.Entry(window, width=30)
@@ -101,7 +90,6 @@ translate_button.pack(padx=10, pady=10)
 output_label = tk.Label(window, text='', font=('Arial', 16))
 output_label.pack(padx=10, pady=10)
 
-# Add a Copy to Clipboard button.
 copy_button = tk.Button(window, text='Copy to Clipboard', command=copy_to_clipboard)
 copy_button.pack(padx=10, pady=10)
 window.mainloop()
